b49f877888d4e1deadf23c1fd39aa0e080ba51c1
Tom Rooker <tomdrooker@gmail.com>
2020-12-04 16:47:59 +0000

    inrange method implemented

--- _.js
+++ _.js
@@ -4,6 +4,22 @@ const _ = {
         const lowerClampedValue = Math.max(number, lower);
         const clampedValue = Math.min(lowerClampedValue, upper);
         return clampedValue;
+    },
+
+    inRange(number, start, end) {
+        if (end === undefined) {
+            end = start;
+            start = 0;
+        };
+
+        if (start > end) {
+            const temp = end;
+            end = start;
+            start = temp;
+        };
+        
+        const isInRange = start <= number && number < end ? true : false;
+        return isInRange; 
     }
 
 };
