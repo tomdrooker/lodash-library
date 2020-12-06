b5fd98c3c8b681892845f41eda45a339ffc09cbb
Tom Rooker <tomdrooker@gmail.com>
2020-12-06 14:23:01 +0000

    chunk method implemented

--- _.js
+++ _.js
@@ -80,6 +80,18 @@ const _ = {
         });
         const droppedArray = this.drop(array, dropNumber);
         return droppedArray;
+    },
+
+    chunk(array, size) {
+        if (size === undefined) {
+            size === 1;
+        };
+        const arrayChunks = [];
+        for(let i = 0; i < array.length; i += size) {
+            let arrayChunk = array.slice(i, (i + size));
+            arrayChunks.push(arrayChunk);
+        }
+        return arrayChunks;
     }
 
 };
